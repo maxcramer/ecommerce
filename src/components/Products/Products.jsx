8843f7f74e555ac4234e21cc5817768970adb256
maxcramer <maximiliancramer@yahoo.com>
2021-04-01 12:20:49 +0100

    repositioning products to sit bellow nav bar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyle from './styles';
 
 const products = [
     {
@@ -35,8 +36,10 @@ const products = [
 ]
  
 const Products = () => {
+    const classes = useStyle();
     return (
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
