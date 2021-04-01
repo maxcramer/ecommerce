7109d7010abf28270845eb6d8f60c2c873b02eea
maxcramer <maximiliancramer@yahoo.com>
2021-04-01 14:51:29 +0100

    hooked up to chec platform, loading products from chec platform database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { commerce } from './lib/commerce';
 
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
@@ -6,10 +7,23 @@ import React from 'react'
 import { Products, Navbar } from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const {data} = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    console.log(products)
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} />
         </div>
     )
 }
